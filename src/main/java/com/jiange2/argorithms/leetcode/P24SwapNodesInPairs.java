1d799e9ec5f925b151c0e26270767b264c341685
jiange2 <992975556@qq.com>
2018-09-02 19:18:08 +0800

    24. 两两交换链表中的节点

--- src/main/java/com/jiange2/argorithms/leetcode/P24SwapNodesInPairs.java
+++ src/main/java/com/jiange2/argorithms/leetcode/P24SwapNodesInPairs.java
@@ -5,6 +5,28 @@ import com.jiange2.struct.ListNode;
 public class P24SwapNodesInPairs {
 
     public ListNode swapPairs(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode t = new ListNode(-1);
+        t.next = head;
+        head = t;
+
+        ListNode cur = head, p1 = head.next, p2 = head.next.next, next = p2.next;
+
+        while (true){
+            cur.next = p2;
+            p2.next = p1;
+            p1.next = next;
+
+            if(next == null || next.next == null) break;
+
+            cur = p1;
+            p1 = next;
+            p2 = next.next;
+            next = p2.next;
+        }
+
+        return head.next;
     }
 }
